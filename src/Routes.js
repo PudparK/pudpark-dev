c2ac6adbaf14a2838bc1b119774671ebfd6981ae
Paul Barron <p.barron@outlook.com>
2020-03-28 15:32:53 -0500

    Added react router. Added header Links for single page functionality. Added home, blog, projects, and resume components. made a routes switch component that contains the content.

---
+++ src/Routes.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Home from "./page/home/Home";
+import Project from "./page/project/Project";
+import Blog from "./page/blog/Blog";
+import Resume from "./page/resume/Resume";
+
+import { Switch, Route } from "react-router-dom";
+
+export default class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/project">
+          <Project />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/resume">
+          <Resume />
+        </Route>
+      </Switch>
+    );
+  }
+}
--- src/component/layout-component/Header/Header.js
+++ src/component/layout-component/Header/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
+
+import logo from "./pk-development-large-bg-white.png";
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -22,19 +25,11 @@ export default class Header extends React.Component {
     return (
       <nav className="flex items-center justify-between flex-wrap p-6">
         <div className="flex items-center flex-shrink-0 mr-6">
-          <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>{" "}
-          <span className="font-semibold text-xl tracking-tight">
-            Tailwind CSS{" "}
-          </span>{" "}
-        </div>{" "}
+          <Link to="/">
+            {" "}
+            <img src={logo} alt="PudparK Developer" />
+          </Link>
+        </div>
         <div className="block lg:hidden" onClick={this.toggleHeader}>
           <button className="flex items-center px-3 py-2 rounded hover:border-white">
             <svg
@@ -42,11 +37,11 @@ export default class Header extends React.Component {
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <title> Menu </title>{" "}
+              <title> Menu </title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>{" "}
-          </button>{" "}
-        </div>{" "}
+            </svg>
+          </button>
+        </div>
         <div
           className={
             "w-full block flex-grow lg:flex lg:items-center lg:w-auto" +
@@ -54,34 +49,28 @@ export default class Header extends React.Component {
           }
         >
           <div className="text-sm lg:flex-grow">
-            <a
-              href="#responsive-header"
+            <Link
               className="block mt-4 lg:inline-block lg:mt-0 mr-4"
+              to="/project"
             >
-              Projects{" "}
-            </a>{" "}
-            <a
-              href="#responsive-header"
+              Projects
+            </Link>
+            <Link
               className="block mt-4 lg:inline-block lg:mt-0 mr-4"
+              to="/blog"
             >
-              Github{" "}
-            </a>{" "}
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0"
-            >
-              Blog{" "}
-            </a>{" "}
-          </div>{" "}
+              Blog
+            </Link>
+          </div>
           <div>
-            <a
-              href="./"
+            <Link
               className="inline-block text-sm px-4 py-2 leading-none border rounded text-red border-red hover:border-transparent hover:bg-white mt-4 lg:mt-0"
+              to="/resume"
             >
-              Resume{" "}
-            </a>{" "}
-          </div>{" "}
-        </div>{" "}
+              Resume
+            </Link>
+          </div>
+        </div>
       </nav>
     );
   }
