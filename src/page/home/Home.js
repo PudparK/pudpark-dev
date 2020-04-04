9e965e54948a68e9179f907303c8357b4b39b88b
Paul Barron <p.barron@outlook.com>
2020-04-04 01:53:56 -0500

    Added Font Awesome icons. Added cursor.

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,18 +1,43 @@
 import React from "react";
+import "./Home.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faLinkedin,
+  faTwitch,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default class Home extends React.Component {
   render() {
     return (
       <>
-        <div className="h-screen flex flex-col justify-center">
-          <h3 className="text-xl">Hello World!</h3>
-          <h1 className="header-text text-red">Paul Barrón</h1>
+        <div className="h-screen flex flex-col justify-center text-gray-100">
+          <h3 className="text-base font-bold">
+            HELLO WORLD<span className="cursor"></span>
+          </h3>
+          <h1 className="header-text text-red font-semibold">Paul Barrón</h1>
           <p className="text-xl">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius harum
             illo dicta adipisci, error, nam reiciendis dolores ut asperiores
             excepturi quia voluptatibus laudantium voluptatum, impedit
             doloremque. Officiis ab deleniti ipsa.
           </p>
+          <ul className="flex justify-around text-red py-6">
+            <li>
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faTwitch} size="2x" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faYoutube} size="2x" />
+            </li>
+          </ul>
         </div>
       </>
     );
