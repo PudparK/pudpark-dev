e2789e9e7b229ce74c55f47a55abd4548cddf643
Paul Barron <p.barron@outlook.com>
2020-05-11 21:03:31 -0500

    .bio-pic background image set correctly.

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -16,18 +16,18 @@ export default class Home extends React.Component {
   render() {
     return (
       <>
-        <div className="pk-container px-0 h-screen flex flex-col justify-center">
+        <div className="pk-container h-screen flex flex-col justify-center">
           <h3 className="text-base font-bold relative lg:text-xl cursor">
             HELLO WORLD
           </h3>
           <h1 className="text-red text-5xl lg:text-6xl font-bold">Paul Barrón</h1>
           <p className="lg:w-3/4 text-3xl lg:text-5xl font-black leading-tight">I started out building <span class="html font-regular">HTML</span> moved on to <span className="css">CSS</span> and now I'm controlling the two w/ <span className="javascript">JS</span>.</p>
 
-          <div className="home-heading flex flex-wrap text-red leading-tight mt-6">
+          <div className="home-heading text-red leading-tight mt-6">
 
-            <ul className="flex items-center text-2xl lg:text-4xl">
-              <li>
-                <Link to="/projects" className="inline-block lg:text-2xl font-semibold px-4 py-2 leading-none border-2 rounded border-red text-red hover:border-transparent hover:bg-red hover:text-black">Projects</Link>
+            <ul className="flex flex-wrap items-center text-3xl lg:text-4xl">
+              <li className="block sm:inline-block">
+                <Link to="/projects" className="block sm:inline-block text-center sm:inline-block lg:text-2xl font-semibold px-4 py-2 border-2 rounded border-red text-red hover:border-transparent hover:bg-red hover:text-black">Projects</Link>
               </li>
               <li>
                 <a href="https://twitter.com/Paul__132">
@@ -52,25 +52,23 @@ export default class Home extends React.Component {
             </ul>
           </div>
         </div>
-        <div className="bio-pic">
-          <div className="container pl-3 pt-10">
-            <FontAwesomeIcon
-              icon={faQuoteLeft}
-              size="2x"
-              className="text-red"
-            />
-            <blockquote className="text-bold text-5xl leading-none pr-40">
-              I like to build cool <br />
+        <div className="pk-container bio-pic">
+          <FontAwesomeIcon
+            icon={faQuoteLeft}
+            size="2x"
+            className="text-red"
+          />
+          <blockquote className="w-1/2 text-5xl leading-none">
+            I like to build cool <br />
               stuff<span className="text-red">.</span>
-              {/* <span className="text-4xl inline-block">
+            {/* <span className="text-4xl inline-block">
                 <FontAwesomeIcon
                   icon={faAsterisk}
                   size="xs"
                   className="text-red flex"
                 />
               </span> */}
-            </blockquote>
-          </div>
+          </blockquote>
         </div>
       </>
     );
