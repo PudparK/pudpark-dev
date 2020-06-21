4993ce5b89e81cbbb953aaf472718415a75f64a8
Paul Barron <p.barron@outlook.com>
2020-06-21 09:18:23 -0500

    Commented out styles for compilation.

--- src/component/layout-component/Post/index.js
+++ src/component/layout-component/Post/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./styles.module.scss";
+// import styles from "./styles.module.scss";
 
 function Post({ data }) {
   return <div>{data.body_markdown}</div>;
