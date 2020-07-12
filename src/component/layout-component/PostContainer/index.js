2b0d15771f6a8ed1511d92c7bf493c1c6972e9b8
PudparK <p.barron@outlook.com>
2020-07-11 19:05:34 -0500

    without null conditional

--- src/component/layout-component/PostContainer/index.js
+++ src/component/layout-component/PostContainer/index.js
@@ -2,34 +2,30 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 function Post({ posts }) {
-  if (!posts) {
-    return <p>un segundo</p>;
-  } else {
-    return (
-      <div className={styles.post}>
-        <div className={styles.cover}>
-          <img src={posts.cover_image} alt="Pudpark Developer" />
+  return (
+    <div className={styles.post}>
+      <div className={styles.cover}>
+        <img src={posts.cover_image} alt="Pudpark Developer" />
+      </div>
+      <div className={styles.postContent}>
+        <div className={styles.articleHeader}>
+          <h2>{posts.title}</h2>
+          <ul className={styles.tagList}>
+            {posts.tag_list.map((tag) => {
+              return <li className={"defaultColor " + tag}>{tag}</li>;
+            })}
+          </ul>
+          <p className={styles.author + " text-red"}>
+            by {posts.user.username.toUpperCase()} on{" "}
+            {new Date(posts.published_at).toLocaleDateString()}
+          </p>
         </div>
-        <div className={styles.postContent}>
-          <div className={styles.articleHeader}>
-            <h2>{posts.title}</h2>
-            <ul className={styles.tagList}>
-              {posts.tag_list.map((tag) => {
-                return <li className={"defaultColor " + tag}>{tag}</li>;
-              })}
-            </ul>
-            <p className={styles.author + " text-red"}>
-              by {posts.user.username.toUpperCase()} on{" "}
-              {new Date(posts.published_at).toLocaleDateString()}
-            </p>
-          </div>
-          <div className={styles.articleBody}>
-            <p>{posts.description}</p>
-          </div>
+        <div className={styles.articleBody}>
+          <p>{posts.description}</p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 function PostContainer({ className, data }) {
