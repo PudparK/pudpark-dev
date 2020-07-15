50a6423865e082b7fb8ac1655f096e1b09b23597
PudparK <p.barron@outlook.com>
2020-07-15 00:21:47 -0500

    removed unused vars.

--- src/page/blog/index.jsx
+++ src/page/blog/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import styles from "./styles.module.scss";
+// import styles from "./styles.module.scss";
 
 // Components
 import Loading from "component/Loading";
--- src/page/project/index.jsx
+++ src/page/project/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import PageTemplate from "component/PageTemplate";
-import PostContainer from "component/PostContainer";
+// import PostContainer from "component/PostContainer";
 
 function Project() {
   return <PageTemplate title="Projects"></PageTemplate>;
